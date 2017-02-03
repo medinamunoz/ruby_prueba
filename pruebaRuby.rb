c7421ce66e2722b11e92315f19f8b9c775b310d6
Mauricio Medina <mauricio.medina.munoz@gmail.com>
2017-02-03 12:32:44 -0300

    a segunda nos fuimos

---
+++ pruebaRuby.rb
@@ -0,0 +1,13 @@
+# 1--Crear un menú con 5 opciones, se debe validar que la opción escogida sea 1, 2, 3, o 4,
+#si el usuario ingresa cualquier otra opción
+#Si el usuario ingresa la opción 4 el programa sale, si ingresa cualquier otra
+#opción se debe mostrar que la opción es inválida, y mostrar el menú
+#nuevamente y la opción de elegir.
+
+
+
+
+era muy malo lo realizado...lo borre.
+debo estudiar más.
+
+un abrazo
